b667c46459690f0455cd1eb8f1d127503a2e6502
hyejinyoo <gpwls4306.g.skku.edu>
2021-08-19 23:02:25 +0900

    Adding auth to our Serverless API

--- list.js
+++ list.js
@@ -11,7 +11,7 @@ export const main = handler(async (event, context) => {
     // 'ExpressionAttributeValues' defines the value in the condition
     // - ':userId': defines 'userId' to be the id of the author
     ExpressionAttributeValues: {
-      ":userID": "123",
+      ":userID": event.requestContext.identity.cognitoIdentityId,
     },
   };
 
